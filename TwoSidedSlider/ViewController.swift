3a2b8308d2e22378e4ca1a0f51a543ac10912e29
Stephen Jones <stephen.jacob.jones@gmail.com>
2015-05-21 10:11:25 -0400

    Initial Commit

---
+++ TwoSidedSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwoSidedSlider
+//
+//  Created by Stephen Jones on 5/21/15.
+//  Copyright (c) 2015 Bits N Grits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
